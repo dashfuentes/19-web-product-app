20da5875101df777bb7fb4dcad72593bc7de19ef
DanyaSantos <santosresendezde1720@gmail.com>
2023-05-08 20:58:51 -0600

    se importa package.json para obtener y mostrar algunos datos de respuesta en la ruta de inicio

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import morgan from 'morgan'
+import pkg from '../package.json'
 const app = express()
+app.set('pkg',pkg)
 app.use(morgan('dev'))
 app.get('/',(req,res)=>{
-    res.json('welcome')
+    res.json({
+        name: app.get('pkg').name,
+        author: app.get('pkg').author,
+        description: app.get('pkg').description,
+        version: app.get('pkg').version
+    })
 })
 export default app
