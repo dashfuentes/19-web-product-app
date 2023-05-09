7bb3281da32d9e8990cd6108fbd6f484b8ef178f
DanyaSantos <santosresendezde1720@gmail.com>
2023-05-08 20:47:29 -0600

    creacion de ruta inicial

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import express from 'express'
+import morgan from 'morgan'
 const app = express()
-
+app.use(morgan('dev'))
+app.get('/',(req,res)=>{
+    res.json('welcome')
+})
 export default app
