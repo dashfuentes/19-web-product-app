38e0a17b54945f04f21892cbd926bd538c8bce73
Emmanuel Reyes <emmanuel.reyes.p@gmail.com>
2023-05-09 21:21:34 -0600

    crud basica

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ app.set("pkg", pkg);
 
 app.use(morgan("dev"));
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({
     name: app.get("pkg").name,
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,21 +1,34 @@
 import Product from '../models/Product.js'
 
-export const createProduct = (req,res) =>{
-    
+export const createProduct = async (req,res) =>{
+    const {name, category, price, imageURL} = req.body
+    const newProduct = new Product({name:name, category:category,price:price,imageUrl:imageURL})
+    const productSaved = await newProduct.save()
+    res.status(201).json(productSaved);
 }
 
-export const getProducts = (req,res) =>{
-    res.json('get Products')
+export const getProducts = async (req,res) =>{
+    const products = await Product.find()
+    res.status(200).json(products)
 }
 
-export const getProductById = (req,res) =>{
-    
+export const getProductById = async (req,res) =>{
+    const {productId} = req.params.productId
+    const product = await Product.findById(productId)
+    res.status(200).json(product)
 }
 
-export const updateProductById = (req,res) =>{
-    
+export const updateProductById = async (req,res) =>{
+    const {productId} = req.params.productId
+    const updatedProduct = await Product.findByIdAndUpdate(productId, req.body, {
+        new:true
+    })
+    res.status(200).json(updatedProduct)
 }
 
-export const deleteProductById = (req,res) =>{
-    
+export const deleteProductById = async (req,res) =>{
+    const {productId} = req.params.productId
+    //const deletedProduct = 
+    await Product.findByIdAndDelete(productId)
+    res.status(204).json() 
 }
