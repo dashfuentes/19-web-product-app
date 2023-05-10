fa8bbf8c065e2920cb2e7f28fa2d4b6be8ec29e0
Ana Vera <apveay@gmail.com>
2023-05-10 17:45:21 -0600

    verificar si el token existe

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -4,18 +4,21 @@ import User from '../models/User.js'
 
 
 export const verifyToken = async (req,res,next) => {
+   try {
     const token = req.headers["x-access-token"]
 
-   console.log(token)
-   if (!token) return res.status(403).json({message: "No token provided"}) 
-   const decoded = json.verify(token, config.SECRET)
-   req.userId = decoded.id
-   
-   const user = await User.findById(req.userId, {password: 0})
-
-   if(!user) return res.status(404).json({message: 'no user found'})
-
-   
+    console.log(token)
+    if (!token) return res.status(403).json({message: "No token provided"}) 
+    const decoded = json.verify(token, config.SECRET)
+    req.userId = decoded.id
+    
+    const user = await User.findById(req.userId, {password: 0})
+ 
+    if(!user) return res.status(404).json({message: 'no user found'})
+ 
+    
+   } catch (error) {
+    return res.status(500).json({message: "unauthoraized"})   }
 
    next()
 }
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,6 +3,7 @@ import {Router} from 'express'
 const router = Router()
 import  * as productCtrl from '../controllers/products.controller.js'
 import middlewares from '../middlewares/index.js'
+import { verifyToken } from '../middlewares/authJwt.js'
 
 
 router.post('/', middlewares.verifyToken, productCtrl.createProduct)
@@ -11,8 +12,8 @@ router.get('/', productCtrl.getProducts)
 
 router.get('/:productId', productCtrl.getProductById)
 
-router.put('/:productId', productCtrl.updateProductById)
+router.put('/:productId', verifyToken, productCtrl.updateProductById)
 
-router.delete('/:productId', productCtrl.deleteProductById)
+router.delete('/:productId', verifyToken, productCtrl.deleteProductById)
 
 export default router
