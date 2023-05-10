ee0fb69df8a965cde6fcaa5e10d2fe9e2c1a550c
DanyaSantos <santosresendezde1720@gmail.com>
2023-05-09 19:39:53 -0600

    logica de ruta para obtener un producto por id

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -9,7 +9,9 @@ export const getProducts = async (req,res)=>{
     const products = await Product.find()
     res.json(products)
 }
-export const getProductById = (req,res)=>{
+export const getProductById = async (req,res)=>{
+    const product = await Product.findById(req.params.productId)
+    res.status(200).json(product)
     
 }
 export const updateProductById = (req,res)=>{
