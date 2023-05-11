345b5ae81d8ec146eec04b7e5494134fa6b801da
Ana Vera <apveay@gmail.com>
2023-05-10 18:40:27 -0600

    check duplicate username or email

--- src/middlewares/verifySignup.js
+++ src/middlewares/verifySignup.js
@@ -1,4 +1,18 @@
 import {ROLES} from '../models/Role.js'
+import User from '../models/User.js'
+
+export const checkDuplicateUsernameOrEmail = async (req,res,next)=>{
+    const user = await User.findOne({username: req.body.username})
+
+    if(user) return res.status(400).json({message: "user already exists"})
+
+    const email = await User.findOne({email: req.body.email})
+    if(email) return res.status(400).json({message: "email already exists"})
+
+    next()
+
+
+}
 
 export const checkRolesExisted = (req, res, next) => {
     if (req.body.roles) {
