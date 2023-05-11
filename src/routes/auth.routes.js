59d2e8fd4d4e3e8c209027b8d0f50cda90282144
Ana Vera <apveay@gmail.com>
2023-05-10 18:43:13 -0600

    verify signup

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import {Router} from 'express'
 const router = Router()
 import * as authCtrl from '../controllers/auth.controller.js'
+import { verifySignup } from '../middlewares/index.js'
 
 
 router.post('/signin', authCtrl.signin)
-router.post('/signup', authCtrl.signup)
+router.post('/signup', [verifySignup.checkDuplicateUsernameOrEmail, verifySignup.checkRolesExisted],authCtrl.signup)
 
 export default router
