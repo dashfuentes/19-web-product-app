72bd9d05896ad86bb1e176c8cab556e92ed94c55
Ana Vera <apveay@gmail.com>
2023-05-10 18:31:26 -0600

    verify if roles exist

---
+++ src/middlewares/verifySignup.js
@@ -0,0 +1,15 @@
+import {ROLES} from './models/Role'
+
+export const checkRolesExisted = (req, res, next) => {
+    if (req.body.roles) {
+        for (let i=0; i<req.body.roles.length; i++){
+            if(!ROLES.includes(req.body.roles[i])){
+                return res.status(400).json({
+                    message: `Role ${req.body.roles[i]} no existe`
+                })
+            }
+        }
+    }
+
+    next()
+}
