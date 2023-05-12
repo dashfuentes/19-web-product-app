0cc70c968c8879390c9755c56fa7cbb831aeaa38
DanyaSantos <santosresendezde1720@gmail.com>
2023-05-11 19:05:16 -0600

    creacion de roles

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import morgan from 'morgan'
 import pkg from '../package.json'
+import { createRoles } from './libs/initialSetup.js'
 import productsRoutes from './routes/products.routes.js'
 import authRoutes from './routes/auth.routes.js'
 const app = express()
-
+createRoles()
 app.set('pkg',pkg)
 app.use(morgan('dev'))
 app.use(express.json())
