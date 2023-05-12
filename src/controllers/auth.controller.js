e4957521a687e0a0a9d6aa76029e13cff0bc746d
DanyaSantos <santosresendezde1720@gmail.com>
2023-05-11 19:23:15 -0600

    asignacion de roles al ingresar

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js'
 import jwt from 'jsonwebtoken'
 import config from '../config.js'
+import Role from '../models/Role.js'
 export const signup = async (req, res)=>{
     const {username,email,password,roles} = req.body
    
@@ -9,7 +10,16 @@ export const signup = async (req, res)=>{
         email,
         password: await User.encryptPassword(password)
     })
+  if (roles){
+    const foundRoles = await Role.find({name: {$in: roles}})
+    newUser.roles = foundRoles.map(role=> role._id)
+  }else{
+    const role = await Role.findOne({name: "user"})
+    newUser.roles = [role._id]
+
+  }
   const savedUser =  await newUser.save()
+  console.log(savedUser)
   const token = jwt.sign({id: savedUser._id},config.SECRET,{
     expiresIn: 86400 //24 horas
   })
